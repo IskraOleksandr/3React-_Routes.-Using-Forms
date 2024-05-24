951d87e082a184fe698f13272e024ac08b3e674d
_Iskra_ <_Iskra_@DESKTOP-4SUHQ4F>
2024-05-24 15:49:56 +0300

    #Task1 Зделано настроено 1 задание

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './App.css';
-
+import './components/Style.css'
+import Nav from './components/Nav'
+import Home from './components/Home'
+import Attractions from './components/Attractions'
+import Photos from './components/Photos'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <div className="app">
+          <Nav />
+          <Routes>
+            <Route path="*" element={<Home />} />
+            <Route path="/attractions" element={<Attractions />} />
+            <Route path="/photos" element={<Photos />} />
+
+          </Routes>
+        </div>
+      </BrowserRouter>
   );
 }
 
---
+++ src/components/Photos.jsx
@@ -0,0 +1,55 @@
+﻿function Photos() {
+  return (
+    <div>
+      <div class="div3">
+        <img
+          class="img2"
+          width="470"
+          src={require('.././img/Vinnickii_Svetlomuzykalynyi_fontan.jpg')}
+          alt="Vinnickii_Svetlomuzykalynyi_fontan"
+        />
+      </div>
+      <div class="div3">
+        <img
+          class="img2"
+          width="470"
+          src={require('../img/museiPirog.jpeg')}
+          alt="Музей_Пирогова"
+        />
+      </div>
+      <div class="div3">
+        <img
+          class="img2"
+          width="470"
+          src={require('.././img/Water_tower2.jpg')}
+          alt="Water_tower2"
+        />
+      </div>
+      <div class="div3">
+        <img
+          class="img2"
+          width="470"
+          src={require('.././img/Vinnicya_Oblrada.jpg')}
+          alt="Vinnicya_Oblrada"
+        />
+      </div>
+      <div class="div3">
+        <img
+          class="img2"
+          width="470"
+          src={require('.././img/museum_vinnytsa.jpg')}
+          alt="museum_vinnytsa"
+        />
+      </div>
+      <div class="div3">
+        <img
+          class="img2"
+          width="470"
+          src={require('.././img/Kempa_Vinnitsa.jpg')}
+          alt="Kempa_Vinnitsa"
+        />
+      </div>
+    </div>
+  );
+}
+export default Photos;
